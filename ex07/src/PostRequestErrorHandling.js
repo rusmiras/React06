f4bdf2b6d07a7e04977289e45823e1be95a381c3
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-05-19 10:53:39 +0200

    Created POST Requests

---
+++ ex07/src/PostRequestErrorHandling.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class PostRequestErrorHandling extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { articleId: null, errorMessage: null};
+    }
+    
+    componentDidMount() {
+        const article={ title: 'React POST Request Example'};
+        
+        axios.post('https://reqres.in/invalid-url', article)
+            .then((response) => this.setState({ articleId: response.data.id }))
+            .catch((error) => {
+                this.setState({ errorMessage: error.message });
+                console.log("There was an error!", error);
+            });
+            
+    }
+    
+    render() {
+        const { errorMessage } = this.state;
+        
+        return (
+            <div className='card text-center m-3'>
+                <h5 className='card-header'>POST Request with Error Handling</h5>
+                <div className='card-body'>Error message: {errorMessage}</div>
+            </div>
+        )
+    }
+}
+
+export default PostRequestErrorHandling;
---
+++ ex08/src/PostRequestSetHeaders.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class PostRequestSetHeaders extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { articleId: null };
+    }
+    
+    componentDidMount() {
+        const article = { title: 'React POST Request Example' };
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer my-token',
+            'My-Custom-Header': 'foobar'
+        };
+        
+        axios.post('https://reqres.in/api/articles', article,{headers})
+            .then(response => this.setState({ articleId: response.data.id }));
+    }
+    
+    render() {
+        const { articleId } = this.state;
+        
+        return (
+            <div className='card text-center m-3'>
+                <h5 className='card-header'>POST Request with Set Headers</h5>
+                <div className='card-body'>Article Id: {articleId}</div>
+            </div>
+        )
+    }
+}
+
+export default PostRequestSetHeaders;
